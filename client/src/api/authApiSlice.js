e4ba6be34be4745a0b9d93d835e951111bb40715
nivwer <nivwerr@gmail.com>
2023-08-13 23:02:34 -0300

    Add endpoints in authApi.

--- client/src/api/authApiSlice.js
+++ client/src/api/authApiSlice.js
@@ -2,11 +2,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApiSlice = createApi({
-  reducerPath: "api",
+  reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/auth",
+    baseUrl: "http://localhost:8000/auth/",
   }),
   endpoints: (builder) => ({
+    // // GetCSRFToken.
+    // getCSRFToken: builder.query({
+    //   query: () => ({
+    //     url: "/token/csrf",
+    //     method: "GET",
+    //   }),
+    // }),
+
     // Create user.
     createUser: builder.mutation({
       query: (newUser) => ({
@@ -15,7 +23,30 @@ export const authApiSlice = createApi({
         body: newUser,
       }),
     }),
+
+    loginUser: builder.mutation({
+      query: (data) => ({
+        url: "token/",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    viewUser: builder.mutation({
+      query: (data) => ({
+        url: "user/",
+        method: "GET",
+        headers: {
+          Authorization: `Token ${data.token}`,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useCreateUserMutation } = authApiSlice;
+export const {
+  useGetCSRFTokenQuery,
+  useCreateUserMutation,
+  useLoginUserMutation,
+  useViewUserMutation,
+} = authApiSlice;
