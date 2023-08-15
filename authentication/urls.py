915c1b5873f6d58122b897616507167eaf5ae77a
nivwer <nivwerr@gmail.com>
2023-08-14 23:29:29 -0300

    fixed: auth paths.

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,8 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    #path('token/csrf/', views.getCSRFToken, name="csrf"),
-    path('token/', obtain_auth_token),
-    path('register/', views.singup, name="register"),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name="signup"),
     path('user/', views.user_view, name="user"),
 ]
