be6b680a3a4901195126850c8a395c9048bdc2ae
nivwer <nivwerr@gmail.com>
2023-08-14 23:27:06 -0300

    Add signin and signup views

--- authentication/views.py
+++ authentication/views.py
@@ -1,32 +1,47 @@
 # Django.
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.middleware.csrf import get_token
-from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Rest Framework.
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework .authentication import SessionAuthentication, TokenAuthentication
 # Serializers.
 from .serializers import UserSerializer
 
 
 # Views.
 
-# async def getCSRFToken(request):
-#     return JsonResponse({'csrfToken': get_token(request)})
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def signin(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"datail": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token": token.key, "user": serializer.data})
 
 
 @api_view(['GET'])
-@permission_required([IsAuthenticated])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def user_view(request):
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
-
-
-async def singup(request):
-    return HttpResponse("SingUp")
-
-
-async def singin(request):
-    return HttpResponse("SingIn")
