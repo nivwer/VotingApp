cbcc2e92c2449346bca3f60f4d8b12deb9f9531f
nivwer <nivwerr@gmail.com>
2023-08-15 23:55:58 -0300

    Update Auth views.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('signin/', views.signin, name='signin'),
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 # Django.
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.db import transaction
 # Rest Framework.
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,6 +10,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework .authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.serializers import ValidationError
 # Serializers.
 from .serializers import UserSerializer
 
@@ -17,26 +18,44 @@ from .serializers import UserSerializer
 # Views.
 
 @api_view(['POST'])
+@transaction.atomic
 def signup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
+    try:
+        serializer = UserSerializer(data=request.data)
+        # Lanza ValidationError si no es válido
+        serializer.is_valid(raise_exception=True)
         serializer.save()
+
         user = User.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
+
         token = Token.objects.create(user=user)
-        return Response({"token": token.key, "user": serializer.data})
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+
+    except ValidationError as e:
+        return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['POST'])
 def signin(request):
-    user = get_object_or_404(User, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"datail": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
+    try:
+        user = get_object_or_404(User, username=request.data['username'])
+
+        if not user.check_password(request.data['password']):
+            return Response({"datail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        
+        return Response({"token": token.key, "user": serializer.data})
+    
+    except ValidationError as e:
+        return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
