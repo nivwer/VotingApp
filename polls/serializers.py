80e5a0c1c1b1d19f8088a44c13bf24c2cbcdb164
nivwer <nivwerr@gmail.com>
2023-08-17 23:08:09 -0300

    Create poll serializer.

---
+++ polls/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+
+class PollSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100, required=True)
+    description = serializers.CharField(max_length=500, required=False)
+    options = serializers.ListField(
+        child=serializers.CharField(max_length=100, required=True)
+    )
+    category = serializers.CharField(max_length=50, required=False)
+
+
+class OptionSerializer(serializers.Serializer):
+    option = serializers.CharField(max_length=100, required=True)
