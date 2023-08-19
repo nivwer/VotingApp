1df36ef8717a611aaa32eda133b17b45500336ee
nivwer <nivwerr@gmail.com>
2023-08-19 00:02:52 -0300

    fixed: PollSerializer.

--- polls/serializers.py
+++ polls/serializers.py
@@ -5,7 +5,8 @@ class PollSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100, required=True)
     description = serializers.CharField(max_length=500, required=False)
     options = serializers.ListField(
-        child=serializers.CharField(max_length=100, required=True)
+        child=serializers.CharField(max_length=100, required=True),
+        required=True
     )
     category = serializers.CharField(max_length=50, required=False)
 
