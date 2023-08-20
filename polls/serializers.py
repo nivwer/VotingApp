28aa68d08cc3d3bd739df128e4d33efbc55fa210
nivwer <nivwerr@gmail.com>
2023-08-20 20:17:11 -0300

    Update polls serializers.

--- polls/serializers.py
+++ polls/serializers.py
@@ -4,12 +4,14 @@ from rest_framework import serializers
 class PollSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100, required=True)
     description = serializers.CharField(max_length=500, required=False)
+    privacy = serializers.CharField(max_length=14, required=False)
+    category = serializers.CharField(max_length=50, required=False)
+
+class OptionsSerializer(serializers.Serializer):
     options = serializers.ListField(
         child=serializers.CharField(max_length=100, required=True),
         required=True
     )
-    category = serializers.CharField(max_length=50, required=False)
-
 
 class OptionSerializer(serializers.Serializer):
-    option = serializers.CharField(max_length=100, required=True)
+    option_text = serializers.CharField(max_length=100, required=True)
