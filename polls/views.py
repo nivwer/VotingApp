4780ad5efb47b5ce6bb2e31d12edd9a8b3ae9b5b
nivwer <nivwerr@gmail.com>
2023-08-20 20:16:05 -0300

    Add user validation in read_poll view.

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from django.http import HttpResponse
 # Rest Framework.
 from rest_framework import status
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, PermissionDenied
 from rest_framework.response import Response
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -17,11 +17,11 @@ from adrf.decorators import api_view
 # MongoDB connection.
 from utils.mongo_connection import MongoDBSingleton
 # MongoDB.
-from pymongo.errors import PyMongoError  
+from pymongo.errors import PyMongoError
 from bson import json_util
 from bson.objectid import ObjectId
 # Serializers.
-from .serializers import PollSerializer
+from .serializers import PollSerializer, OptionsSerializer
 
 
 # Load the virtual environment.
@@ -50,7 +50,7 @@ async def read_poll(request):
     # If poll_id is undefined.
     if not poll_id:
         raise ValidationError(
-                {'poll_id': ["This field is required."]})
+            {'poll_id': ["This field is required."]})
     try:
         # Get collections from the polls database.
         polls_db = GetCollectionsMongoDB('polls_db', ["polls"])
@@ -62,6 +62,14 @@ async def read_poll(request):
             raise ValidationError(
                 {'poll_id': ["Poll is not found."]})
 
+        # If user is not authorized.
+        if poll_bson["created_by"]["user_id"] != request.user.id:
+            raise ValidationError(
+                {"error": "You are not authorized to read this poll."})
+
+        # If privacy of poll is private. ?
+        # If privacy of poll is friends_only. ?
+
         # Convert the BSON response to a JSON response.
         poll_json = json_util._json_convert((poll_bson))
         # Fix data.
@@ -70,11 +78,15 @@ async def read_poll(request):
 
         # Return a JSON with the poll.
         return Response({"poll": poll_json})
-    
+
     # Handle validation errors.
     except ValidationError as e:
         return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
-    
+
+    # Handle permission denied.
+    except PermissionDenied as e:
+        return Response({"error": e.detail}, status=status.HTTP_403_FORBIDDEN)
+
     # Handle MongoDB errors.
     except PyMongoError as e:
         print(f'MongoDB Error: {e}')
@@ -93,17 +105,20 @@ async def read_poll(request):
 async def create_poll(request):
     session = None
     try:
-        # Initialize a PollSerializer instance with the provided data.
-        serializer = PollSerializer(data=request.data)
+        # Initialize a Serializers instance with the provided data.
+        poll_serializer = PollSerializer(data=request.data)
+        options_serializer = OptionsSerializer(data=request.data)
         # Throws ValidationError if not valid.
-        serializer.is_valid(raise_exception=True)
+        poll_serializer.is_valid(raise_exception=True)
+        options_serializer.is_valid(raise_exception=True)
 
         # Fields requireds.
-        title = serializer.validated_data.get('title')
-        options = serializer.validated_data.get('options')
+        title = poll_serializer.validated_data.get('title')
+        options = options_serializer.validated_data.get('options')
         # If the value is undefined, the value is null.
-        description = serializer.validated_data.get('description')
-        category = serializer.validated_data.get('category')
+        description = poll_serializer.validated_data.get('description')
+        privacy = poll_serializer.validated_data.get('privacy')
+        category = poll_serializer.validated_data.get('category')
 
         # If the list options do not have a length of two or more items.
         if len(options) <= 1:
@@ -112,7 +127,12 @@ async def create_poll(request):
         elif len(options) >= 18:
             raise ValidationError(
                 {'options': ["This field expects a list with less than 18 elements."]})
-        
+
+        # If privacy is not valid or undefined.
+        privacy_list = ["public", "private", "friends_only"]
+        if not privacy in privacy_list:
+            privacy = "public"
+
         # Initialize a MongoDB session.
         async with await MongoDBSingleton().client.start_session() as session:
             # Initialize a MongoDB transaccion.
@@ -131,10 +151,10 @@ async def create_poll(request):
                         },
                         "title": title,
                         "description": description,
-                        "creation_date": datetime.now().strftime("[%d/%b/%Y %H:%M:%S]"),
+                        "creation_date": datetime.now(),
                         "total_votes": 0,
                         "voters": [],
-                        "privacy": "public",
+                        "privacy": privacy,
                         "category": category
                     },
                     session=session
@@ -182,7 +202,7 @@ async def create_poll(request):
     # Handle validation errors.
     except ValidationError as e:
         return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
-    
+
     # Handle MongoDB errors.
     except PyMongoError as e:
         print(f'MongoDB Error: {e}')
@@ -202,8 +222,59 @@ async def create_poll(request):
             await session.end_session()
 
 
+@api_view(['PATCH'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 async def update_poll(request):
-    return Response("Poll Updated")
+    poll_id = request.data["poll_id"]
+    # If poll_id is undefined.
+    if not poll_id:
+        raise ValidationError(
+            {'poll_id': ["This field is required."]})
+    try:
+        # Get collections from the polls database.
+        polls_db = GetCollectionsMongoDB('polls_db', ["polls"])
+        # Find the poll in the polls collection.
+        poll_bson = await polls_db.polls.find_one({"_id": ObjectId(poll_id)})
+
+        # If poll is not found.
+        if not poll_bson:
+            raise ValidationError(
+                {'poll_id': ["Poll is not found."]})
+
+        # If user is not authorized.
+        if poll_bson["created_by"]["user_id"] != request.user.id:
+            raise ValidationError(
+                {"error": "You are not authorized to read this poll."})
+
+        # Initialize a PollSerializer instance with the provided data.
+        serializer = PollSerializer(data=request.data, partial=True)
+        # Throws ValidationError if not valid.
+        serializer.is_valid(raise_exception=True)
+
+        # Get the updated fields.
+        data = serializer.validated_data
+        # Update poll document in polls collection.
+        await polls_db.polls.update_one(
+            {"_id": ObjectId(poll_id)}, {"$set": data})
+
+        # Response.
+        return Response({"message": "Poll updated successfully."}, status=status.HTTP_200_OK)
+
+    # Handle validation errors.
+    except ValidationError as e:
+        return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Handle MongoDB errors.
+    except PyMongoError as e:
+        print(f'MongoDB Error: {e}')
+        return Response({"error": "An error occurred while processing your request."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    # Handle other exceptions.
+    except Exception as e:
+        print(f'Error: {e}')
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 async def delete_poll(request):
     return Response("Poll removed")
