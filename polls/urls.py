7889cea5008307c5d1b8632dac9708bf1d16bb56
nivwer <nivwerr@gmail.com>
2023-08-21 03:52:29 -0300

    Update auth urls.

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('create/', views.create_poll, name='CreatePoll'),
     path('read/', views.read_poll, name='ReadPoll'),
-    path('update/', views.update_poll, name='UpdatePoll')
+    path('update/', views.update_poll, name='UpdatePoll'),
+    path('delete/', views.delete_poll, name='DeletePoll')
 ]
