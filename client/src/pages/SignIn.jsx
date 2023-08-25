5f1b7c830b3841cd3b17a079be45c18b34d40210
nivwer <nivwerr@gmail.com>
2023-08-24 23:37:55 -0300

    Add styles to SignIn and SignUp pages.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,4 +1,5 @@
 // Hooks.
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
@@ -6,8 +7,12 @@ import { useSignInMutation } from "../api/authApiSlice";
 // Actions.
 import { login } from "../features/auth/authSlice";
 //Components.
-import { Container, Center, Flex } from "@chakra-ui/react";
 import {
+  Container,
+  Center,
+  Flex,
+  FormErrorMessage,
+  FormHelperText,
   Box,
   Button,
   FormControl,
@@ -16,7 +21,12 @@ import {
   Stack,
   Text,
   useColorMode,
+  InputGroup,
+  InputRightElement,
+  Progress,
 } from "@chakra-ui/react";
+// Icons.
+import { ViewOffIcon, ViewIcon } from "@chakra-ui/icons";
 
 // Page.
 function SignIn() {
@@ -36,23 +46,39 @@ function SignIn() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
+    setError,
   } = useForm();
+
+  // Show Password field.
+  const [showPassword, setShowPassword] = useState(false);
 
   // Submit.
   const onSubmit = handleSubmit(async (data) => {
     try {
       const res = await signIn(data);
+      // If the authentication is valid.
+      if (res.data) {
+        dispatch(
+          login({
+            token: res.data.token,
+            user: res.data.user,
+          })
+        );
+        navigate("/home");
+      }
 
-      dispatch(
-        login({
-          token: res.data.token,
-          user: res.data.user,
-        })
-      );
-
-      navigate("/user/polls");
+      if (res.error) {
+        if (res.error.data.username) {
+          setError("username", {
+            message: res.error.data.username[0],
+          });
+        } else if (res.error.data.password) {
+          setError("password", {
+            message: res.error.data.password[0],
+          });
+        }
+      }
     } catch (error) {
       console.error(error);
     }
@@ -60,74 +86,118 @@ function SignIn() {
 
   return (
     <>
-      <Container maxW="sm">
-        <Center h="100vh">
+      <Container maxW="md">
+        <Center minH="calc(100vh - 200px)">
           <Box
-            p="10"
-            px="14"
-            bg={isDark ? `black` : `${color}.25` }
+            w={"100%"}
+            bg={isDark ? `black` : `${color}.25`}
             outline={isDark ? "1px solid" : "2px solid"}
             outlineColor={isDark ? `${color}.200` : `${color}.500`}
             borderRadius="14px"
             textAlign="center"
           >
-            <form>
-              <Stack spacing={6}>
-                <Text fontSize="3xl" fontWeight="bold">
-                  Sign In
-                </Text>
-                <Stack spacing={3}>
-                  <FormControl>
-                    <FormLabel>Username</FormLabel>
-                    <Input type="text" />
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel>Password</FormLabel>
-                    <Input type="password" />
-                  </FormControl>
+            {isLoading && (
+              <Progress
+                colorScheme={color}
+                borderTopRadius="14px"
+                m="auto"
+                w={"98%"}
+                size="xs"
+                isIndeterminate
+              />
+            )}
+            <Box p="14" pb="10" px="8%">
+              <form onSubmit={onSubmit}>
+                <Stack spacing={6}>
+                  <Text fontSize="3xl" fontWeight="bold">
+                    Sign In
+                  </Text>
+                  <Stack textAlign="start" spacing={3}>
+                    {/* username. */}
+                    <FormControl
+                      isDisabled={isLoading}
+                      isInvalid={errors.username}
+                    >
+                      <FormLabel fontWeight={"bold"} htmlFor="username">
+                        Username
+                      </FormLabel>
+                      <Input
+                        focusBorderColor={
+                          isDark ? `${color}.200` : `${color}.500`
+                        }
+                        placeholder="Username"
+                        {...register("username", {
+                          required: {
+                            value: true,
+                            message: "Username is required.",
+                          },
+                        })}
+                        type="text"
+                      />
+                      {/* Handle errors. */}
+                      {errors.username && (
+                        <FormErrorMessage>
+                          {errors.username.message}
+                        </FormErrorMessage>
+                      )}
+                    </FormControl>
+                    {/* Password. */}
+                    <FormControl
+                      isDisabled={isLoading}
+                      isInvalid={errors.password}
+                    >
+                      <FormLabel fontWeight={"bold"} htmlFor="password">
+                        Password
+                      </FormLabel>
+                      <InputGroup size="md">
+                        <Input
+                          focusBorderColor={
+                            isDark ? `${color}.200` : `${color}.500`
+                          }
+                          placeholder="Password"
+                          {...register("password", {
+                            required: {
+                              value: true,
+                              message: "Password is required.",
+                            },
+                          })}
+                          type={showPassword ? "text" : "password"}
+                        />
+                        <InputRightElement width="3rem">
+                          <Button
+                            isDisabled={isLoading}
+                            colorScheme={color}
+                            variant={"unstyled"}
+                            h="1.75rem"
+                            size="sm"
+                            onClick={() => {
+                              setShowPassword(!showPassword);
+                            }}
+                          >
+                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      {errors.password && (
+                        <FormErrorMessage>
+                          {errors.password.message}
+                        </FormErrorMessage>
+                      )}
+                    </FormControl>
+                  </Stack>
+                  <Flex justifyContent="center">
+                    <Button
+                      isDisabled={isLoading}
+                      type="submit"
+                      colorScheme={color}
+                      variant="solid"
+                    >
+                      Sign In
+                    </Button>
+                  </Flex>
                 </Stack>
-                <Flex justifyContent="center">
-                  <Button type="submit" 
-                  colorScheme={color}
-                  variant="solid">Sign In</Button>
-                </Flex>
-              </Stack>
-            </form>
-
-            {/* <form onSubmit={onSubmit}> */}
-            {/* Name. */}
-            {/* <label htmlFor="username">Username</label>
-              <input
-                {...register("username", {
-                  required: {
-                    value: true,
-                    message: "Name is required",
-                  },
-                  minLength: {
-                    value: 4,
-                    message: "Min 4 digits",
-                  },
-                })}
-                type="text"
-              /> */}
-            {/* Handle errors. */}
-            {errors.name && <span>{errors.name.message}</span>}
-
-            {/* Password. */}
-            {/* <label htmlFor="password">Password</label>
-              <input
-                {...register("password", {
-                  required: {
-                    value: true,
-                    message: "Password is required",
-                  },
-                })}
-                type="password"
-              /> */}
-            {/* Handle errors. */}
-            {/* {errors.password && <span>{errors.password.message}</span>}
-              <button type="submit">Submit</button>
-            </form> */}
+              </form>
+            </Box>
           </Box>
         </Center>
       </Container>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,44 @@
 // Hooks.
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useSignUpMutation } from "../api/authApiSlice";
 // Actions.
 import { login } from "../features/auth/authSlice";
 // Components.
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Progress,
+  Stack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+
+// Icons.
+import { ViewOffIcon, ViewIcon } from "@chakra-ui/icons";
 
 // Page.
 function SignUp() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  // Theme color.
+  const theme = useSelector((state) => state.theme);
+  const color = theme.theme_color;
+  // Theme mode.
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   const [signUp, { isLoading, isError }] = useSignUpMutation();
 
   // React hook form.
@@ -19,14 +47,42 @@ function SignUp() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm();
+    setError,
+  } = useForm({
+    criteriaMode: "all",
+  });
 
-  // Send user data.
+  // Show Password fields.
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  // Submit.
   const onSubmit = handleSubmit(async (data) => {
     try {
       const res = await signUp(data);
-      dispatch(login({ token: res.data.token }));
-      navigate("/user/polls");
+      // If the registration is valid.
+      if (res.data) {
+        dispatch(
+          login({
+            token: res.data.token,
+            user: res.data.user,
+          })
+        );
+        navigate("/home");
+      }
+
+      // If server error.
+      if (res.error) {
+        if (res.error.data.username) {
+          setError("username", {
+            message: res.error.data.username[0],
+          });
+        } else if (res.error.data.password) {
+          setError("password", {
+            message: res.error.data.password[0],
+          });
+        }
+      }
     } catch (error) {
       console.error(error);
     }
@@ -34,60 +90,191 @@ function SignUp() {
 
   return (
     <>
-      {/* Create user form. */}
-      <form onSubmit={onSubmit}>
-        {/* Name. */}
-        <label htmlFor="username">Username</label>
-        <input
-          {...register("username", {
-            required: {
-              value: true,
-              message: "Name is required",
-            },
-            minLength: {
-              value: 4,
-              message: "Min 4 digits",
-            },
-          })}
-          type="text"
-        />
-        {/* Handle errors. */}
-        {errors.name && <span>{errors.name.message}</span>}
-
-        {/* Password. */}
-        <label htmlFor="password">Password</label>
-        <input
-          {...register("password", {
-            required: {
-              value: true,
-              message: "Password is required",
-            },
-          })}
-          type="password"
-        />
-        {/* Handle errors. */}
-        {errors.password && <span>{errors.password.message}</span>}
-
-        {/* Password Validation. */}
-        <label htmlFor="passwordValidation">Confirm Password</label>
-        <input
-          {...register("passwordValidation", {
-            required: {
-              value: true,
-              message: "Password is required",
-            },
-            validate: (value) =>
-              value === watch("password") || "Password not match",
-          })}
-          type="password"
-        />
-        {/* Handle errors. */}
-        {errors.passwordValidation && (
-          <span>{errors.passwordValidation.message}</span>
-        )}
-
-        <button type="submit">Submit</button>
-      </form>
+      <Container maxW="xl">
+        <Center minH="calc(100vh - 200px)">
+          <Box
+            w={"100%"}
+            bg={isDark ? `black` : `${color}.25`}
+            outline={isDark ? "1px solid" : "2px solid"}
+            outlineColor={isDark ? `${color}.200` : `${color}.500`}
+            borderRadius="14px"
+            textAlign="center"
+          >
+            {isLoading && (
+              <Progress
+                colorScheme={color}
+                borderTopRadius="14px"
+                m="auto"
+                w={"98%"}
+                size="xs"
+                isIndeterminate
+              />
+            )}
+            <Box p="14" pb="10" px="8%">
+              {/* Create user form. */}
+              <form onSubmit={onSubmit}>
+                <Stack spacing={8}>
+                  <Text fontSize="3xl" fontWeight="bold">
+                    Sign Up
+                  </Text>
+                  <Stack textAlign="start" spacing={3}>
+                    {/* username. */}
+                    <FormControl
+                      isDisabled={isLoading}
+                      isInvalid={errors.username}
+                    >
+                      <FormLabel fontWeight={"bold"} htmlFor="username">
+                        Username
+                      </FormLabel>
+                      <Input
+                        focusBorderColor={
+                          isDark ? `${color}.200` : `${color}.500`
+                        }
+                        placeholder="Username"
+                        {...register("username", {
+                          required: {
+                            value: true,
+                            message: "Username is required.",
+                          },
+                          maxLength: {
+                            value: 32,
+                            message: "Max 32 digits.",
+                          },
+                          minLength: {
+                            value: 3,
+                            message: "Min 3 digits.",
+                          },
+                        })}
+                        type="text"
+                      />
+                      {/* Handle errors. */}
+                      {errors.username && (
+                        <FormErrorMessage>
+                          {errors.username.message}
+                        </FormErrorMessage>
+                      )}
+                    </FormControl>
+                    {/* Password. */}
+                    <FormControl
+                      isDisabled={isLoading}
+                      isInvalid={errors.password}
+                    >
+                      <FormLabel fontWeight={"bold"} htmlFor="password">
+                        Password
+                      </FormLabel>
+                      <InputGroup size="md">
+                        <Input
+                          focusBorderColor={
+                            isDark ? `${color}.200` : `${color}.500`
+                          }
+                          placeholder="Password"
+                          {...register("password", {
+                            required: {
+                              value: true,
+                              message: "Password is required.",
+                            },
+                            maxLength: {
+                              value: 96,
+                              message: "Max 96 digits.",
+                            },
+                            minLength: {
+                              value: 8,
+                              message: "Min 8 digits.",
+                            },
+                          })}
+                          type={showPassword ? "text" : "password"}
+                        />
+                        <InputRightElement width="3rem">
+                          <Button
+                            isDisabled={isLoading}
+                            colorScheme={color}
+                            variant={"unstyled"}
+                            h="1.75rem"
+                            size="sm"
+                            onClick={() => {
+                              setShowPassword(!showPassword);
+                            }}
+                          >
+                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      {errors.password && (
+                        <FormErrorMessage>
+                          {errors.password.message}
+                        </FormErrorMessage>
+                      )}
+                    </FormControl>
+                    {/* Password Validation. */}
+                    <FormControl
+                      isDisabled={isLoading}
+                      isInvalid={errors.passwordValidation}
+                    >
+                      <FormLabel
+                        fontWeight={"bold"}
+                        htmlFor="passwordValidation"
+                      >
+                        Confirm Password
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          focusBorderColor={
+                            isDark ? `${color}.200` : `${color}.500`
+                          }
+                          placeholder="Confirm password"
+                          {...register("passwordValidation", {
+                            required: {
+                              value: true,
+                              message: "Is required.",
+                            },
+                            validate: (value) =>
+                              value === watch("password") ||
+                              "Password not match.",
+                          })}
+                          type={showConfirmPassword ? "text" : "password"}
+                        />
+                        <InputRightElement width="3rem">
+                          <Button
+                            isDisabled={isLoading}
+                            colorScheme={color}
+                            variant={"unstyled"}
+                            h="1.75rem"
+                            size="sm"
+                            onClick={() => {
+                              setShowConfirmPassword(!showConfirmPassword);
+                            }}
+                          >
+                            {showConfirmPassword ? (
+                              <ViewIcon />
+                            ) : (
+                              <ViewOffIcon />
+                            )}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      {errors.passwordValidation && (
+                        <FormErrorMessage>
+                          {errors.passwordValidation.message}
+                        </FormErrorMessage>
+                      )}
+                    </FormControl>
+                  </Stack>
+                  <Flex justifyContent="center">
+                    <Button
+                      isDisabled={isLoading}
+                      type="submit"
+                      colorScheme={color}
+                      variant="solid"
+                    >
+                      Sign Up
+                    </Button>
+                  </Flex>
+                </Stack>
+              </form>
+            </Box>
+          </Box>
+        </Center>
+      </Container>
     </>
   );
 }
