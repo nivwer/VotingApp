8c2f2899af7d1c97829c7c80a72fe5d144f6c4a5
nivwer <nivwerr@gmail.com>
2023-08-24 23:36:39 -0300

    Update auth serializers.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,3 +5,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def validate_username(self, value):
+        if len(value) < 3:  
+            raise serializers.ValidationError("Min 3 digits.")
+        if len(value) > 32:  
+            raise serializers.ValidationError("Max 32 digits.")
+        return value
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Min 8 digits.")
+        if len(value) > 98:
+            raise serializers.ValidationError("Max 96 digits.")
+        return value
+    
+    def validate_first_name(self, value):
+        if len(value) < 3:  
+            raise serializers.ValidationError("Min 3 digits.")
+        if len(value) > 32:  
+            raise serializers.ValidationError("Max 32 digits.")
+        return value
