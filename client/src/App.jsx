1ca981f643b9d546dc951f24358750eebfee9ad5
nivwer <nivwerr@gmail.com>
2023-08-27 19:50:52 -0300

    Update routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-// Router component.
-import Router from './routes/Router'
+// Hooks.
+import { useSelector } from "react-redux";
+// Components..
+import Router from "./routes/Router";
+import { BrowserRouter } from "react-router-dom";
+import {  useColorMode, Container } from "@chakra-ui/react";
 
+// App.
 function App() {
+  // Theme color.
+  const theme = useSelector((state) => state.theme);
+  const color = theme.theme_color;
+  // Theme mode.
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
-    <Router />
+    <BrowserRouter>
+      <Container
+        p={"0"}
+        maxW="100vw"
+        minH="100vh"
+        bg={isDark ? "black" : `${color}.bg-l-p`}
+        color={isDark ? `${color}.100` : `${color}.900`}
+      >
+        <Router />
+      </Container>
+    </BrowserRouter>
   );
 }
 
--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,77 +1,62 @@
 // React Router Components.
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // Hooks.
 import { useSelector } from "react-redux";
-import { useColorMode } from "@chakra-ui/react";
 // Pages.
 import Home from "../pages/Home";
 import About from "../pages/About";
 import SignUp from "../pages/Auth/SignUp";
 import SignIn from "../pages/Auth/SignIn";
 import NotFoundPage from "../pages/NotFoundPage";
-import Profile from "../pages/user/Profile";
-import ViewPoll from "../pages/polls/ViewPoll";
-import NewPoll from "../pages/polls/NewPoll";
+import Profile from "../pages/User/Profile";
+import ViewPoll from "../pages/Polls/ViewPoll";
+import NewPoll from "../pages/Polls/NewPoll";
+import Theme from "../pages/Settings/Theme";
 // Components.
 import ProtectedRoute from "./ProtectedRoute";
-import Navbar from "../components/Navigation/Navbar";
-import { Container } from "@chakra-ui/react";
-import Theme from "../pages/config/Theme";
+import GridLayout from "../components/Layout/GridLayout";
 
 // Router.
 function Router() {
-  // Theme color.
-  const theme = useSelector((state) => state.theme);
-  const color = theme.theme_color;
-  // Theme mode.
-  const { colorMode } = useColorMode();
-  const isDark = colorMode === "dark";
   const data = useSelector((state) => state.session);
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Container
-        p={"0"}
-        w="100vw"
-        maxW="100vw"
-        minH="calc(100vh - 100px)"
-        mt={"100px"}
-        bg={isDark ? "black" : `${color}.bg-l-p`}
-        color={isDark ? `${color}.100` : `${color}.900`}
-      >
-        <Routes>
-          {/* ### PUBLIC ROUTES ### */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          {/* Profile page. */}
-          <Route path="/:username" element={<Profile />} />
-          {/* Poll page. */}
-          <Route path="/:username/:id" element={<ViewPoll />} />
-          {/* Authentication Pages. */}
-          <Route element={<ProtectedRoute isAllowed={!data.token} />}>
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-          </Route>
-          {/* ### PUBLIC ROUTES. ### */}
+    <Routes>
+      {/* Simple Layout. */}
+      <Route element={<GridLayout />}>
+        {/* Authentication Pages. */}
+        <Route element={<ProtectedRoute isAllowed={!data.token} />}>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Route>
 
-          {/* ### PRIVATE ROUTES. ### */}
-          {/* Poll pages. */}
-          <Route element={<ProtectedRoute isAllowed={!!data.token} />}>
-            <Route path="/:username/new" element={<NewPoll />} />
-          </Route>
-          {/* Config pages. */}
-          <Route element={<ProtectedRoute isAllowed={!!data.token} />}>
-            {/*  <Route path="/config" element={<Config />} /> */}
-            <Route path="/config/theme" element={<Theme />} />
-          </Route>
-          {/* ### PRIVATE ROUTES. ### */}
+        {/* About page. */}
+        <Route path="/about" element={<About />} />
 
-          {/* Page 404. */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
+        {/* Page 404. */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+
+      {/* Triple Columns Layout. */}
+      <Route element={<GridLayout isSimple={false} />}>
+        {/* Home page. */}
+        <Route path="/home" element={<Home />} />
+        {/* Profile page. */}
+        <Route path="/:username" element={<Profile />} />
+        {/* Poll page. */}
+        <Route path="/:username/:id" element={<ViewPoll />} />
+
+        {/* Poll pages. */}
+        <Route element={<ProtectedRoute isAllowed={!!data.token} />}>
+          <Route path="/:username/new" element={<NewPoll />} />
+        </Route>
+        {/* Config pages. */}
+        <Route element={<ProtectedRoute isAllowed={!!data.token} />}>
+          {/*  <Route path="/settings" element={<Settings />} /> */}
+          <Route path="/settings/theme" element={<Theme />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
