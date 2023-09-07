f696337facd28e6f2da319fd631bffc35d9231c5
nivwer <nivwerr@gmail.com>
2023-09-07 20:40:18 -0300

    Add Profiles creation.

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,28 +1,32 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
     def validate_username(self, value):
-        if len(value) < 3:  
-            raise serializers.ValidationError("Minimum 18 options allowed.")
-        if len(value) > 32:  
-            raise serializers.ValidationError("Maximum 32 options allowed.")
+        if len(value) < 3:
+            raise serializers.ValidationError("Minimum 3 characters allowed.")
+        if len(value) > 32:
+            raise serializers.ValidationError("Maximum 32 characters allowed.")
         return value
 
     def validate_password(self, value):
         if len(value) < 8:
-            raise serializers.ValidationError("Minimum 8 options allowed.")
-        if len(value) > 98:
-            raise serializers.ValidationError("Maximum 96 options allowed.")
+            raise serializers.ValidationError("Minimum 8 characters allowed.")
+        if len(value) > 10000:
+            raise serializers.ValidationError("Maximum 96 characters allowed.")
         return value
-    
-    def validate_first_name(self, value):
-        if len(value) < 3:  
-            raise serializers.ValidationError("Min 3 digits.")
-        if len(value) > 32:  
-            raise serializers.ValidationError("Max 32 digits.")
-        return value
+
+    # def validate_first_name(self, value):
+    #     if len(value) > 32:
+    #         raise serializers.ValidationError("Maximum 32 characters allowed.")
+    #     return value
+
+    # def validate_last_name(self, value):
+    #     if len(value) > 32:
+    #         raise serializers.ValidationError("Maximum 32 characters allowed.")s
+    #     return value
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,6 @@
 # Standard.
 from datetime import datetime
 # Django.
-from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.db import transaction
 # Rest Framework.
@@ -13,7 +12,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.models import Token
 # Models and Serializers.
+from apps.profiles.models import UserProfile
 from .serializers import UserSerializer
+from apps.profiles.serializers import UserProfileSerializer
 
 
 # Views.
@@ -23,42 +24,64 @@ from .serializers import UserSerializer
 @transaction.atomic
 def signup(request):
     try:
-        # Config the data.
-        data = {
-            "username": request.data["username"],
-            "password": request.data["password"],
-            "first_name": request.data["username"],
-            "last_login": datetime.now(),
+        # Config the data object.
+        user_object = {
+            'username': request.data['username'],
+            'password': request.data['password'],
+            'last_login': datetime.now(),
         }
 
         # Initialize a UserSerializer instance with the provided data.
-        serializer = UserSerializer(data=data)
+        user_serializer = UserSerializer(data=user_object)
         # Throws ValidationError if not valid.
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        user_serializer.is_valid(raise_exception=True)
+        user_instance = user_serializer.save()
 
         # Set the password for the user using the provided password.
-        user = User.objects.get(username=serializer.data['username'])
-        user.set_password(serializer.data['password'])
-        user.save()
+        user_object = User.objects.get(
+            username=user_serializer.data['username'])
+        user_object.set_password(user_serializer.data['password'])
+        user_object.save()
 
         # Create a new Token instance associated with the user.
-        token = Token.objects.create(user=user)
+        token = Token.objects.create(user=user_object)
 
-        # Remove the 'password' field
-        user_data = serializer.data
+        # Remove the 'password' field to the response.
+        user_data = user_serializer.data
         user_data.pop('password')
 
-        # Response if successful signup.
-        return Response({'token': token.key, 'user': user_data}, status=status.HTTP_201_CREATED)
+        # Create a object to ProfileSerializer.
+        profile_object = {
+            'user': user_instance.pk,
+            'profile_name': request.data['username']
+        }
+        # Initialize a ProfileSerializer instance.
+        profile_serializer = UserProfileSerializer(data=profile_object)
+        # Throws ValidationError if not valid.
+        profile_serializer.is_valid(raise_exception=True)
+        profile_serializer.save()
+        profile_data = profile_serializer.data
+
+        # Response.
+        return Response(
+            {
+                'token': token.key,
+                'user': user_data,
+                'profile': profile_data,
+            },
+            status=status.HTTP_201_CREATED)
 
     # Handle validation errors.
     except ValidationError as e:
+        print(e.detail)
         return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
 
     # Handle other exceptions.
     except Exception as e:
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        print(str(e))
+        return Response(
+            {"error": str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 # Handles the user authentication process.
@@ -72,18 +95,33 @@ def signin(request):
         # Check if the password matches the user's stored password.
         if not user.check_password(request.data['password']):
             # If the password does not match return the 401 response.
-            raise AuthenticationFailed({'password': ['Invalid password.']})
+            raise AuthenticationFailed(
+                {'password': ['Invalid password.']})
 
         # Get token associated with the user, or create a new token.
         token, created = Token.objects.get_or_create(user=user)
 
-        # Response if successful signin.
-        serializer = UserSerializer(instance=user)
-        # Remove the 'password' field
-        user_data = serializer.data
+        # Initialize a UserSerializer instance.
+        user_serializer = UserSerializer(instance=user)
+        user_pk = user_serializer.instance.pk
+        # Remove the 'password' field to the response.
+        user_data = user_serializer.data
         user_data.pop('password')
 
-        return Response({'token': token.key, 'user': user_data})
+        # Get user profile.
+        user_profile = UserProfile.objects.get(user=user_pk)
+        # Initialize a ProfileSerializer instance.
+        profile_data = UserProfileSerializer(
+            instance=user_profile).data
+
+        # Response.
+        return Response(
+            {
+                'token': token.key,
+                'user': user_data,
+                'profile': profile_data,
+            },
+            status=status.HTTP_200_OK)
 
     # Handle validation errors.
     except ValidationError as e:
@@ -95,11 +133,15 @@ def signin(request):
 
     # Handle if the user is not found.
     except User.DoesNotExist:
-        return Response({'username': ['User is not found.']}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {'username': ['User is not found.']},
+            status=status.HTTP_404_NOT_FOUND)
 
     # Handle other exceptions.
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(
+            {'error': str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
