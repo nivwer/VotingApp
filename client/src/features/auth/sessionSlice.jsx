dd408a7047b63dd5add74abb244b664e6f9c09cf
nivwer <nivwerr@gmail.com>
2023-09-09 21:53:56 -0300

    Rename: authSlice to sessionSlice.

--- client/src/features/auth/sessionSlice.jsx
+++ client/src/features/auth/sessionSlice.jsx
@@ -6,23 +6,29 @@ const initialState = {
   profile: null,
 };
 
-export const authSlice = createSlice({
+export const sessionSlice = createSlice({
   name: "session",
   initialState,
   reducers: {
+    // Action to login.
     login: (state, action) => {
       state.token = action.payload.token;
       state.user = action.payload.user;
       state.profile = action.payload.profile;
     },
+    // Action to logout.
     logout: (state) => {
       state.token = null;
       state.user = null;
       state.profile = null;
     },
+    // Action to update profile.
+    updateProfile: (state, action) => {
+      state.profile = action.payload.profile;
+    },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, updateProfile } = sessionSlice.actions;
 
-export default authSlice.reducer;
+export default sessionSlice.reducer;
