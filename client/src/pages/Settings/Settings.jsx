ac3c82a208674fa35c5314d447c6dd836645591f
nivwer <nivwerr@gmail.com>
2023-10-11 14:23:43 -0300

    Add settings page.

--- client/src/pages/Settings/Settings.jsx
+++ client/src/pages/Settings/Settings.jsx
@@ -1,21 +1,60 @@
+// Hooks.
+import { useThemeInfo } from "../../hooks/Theme";
 // Components.
-import { Button, Stack } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Divider,
+  HStack,
+  Icon,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+// Icons.
+import {
+  FaUserLock,
+  FaUserGear,
+  FaUserPen,
+  FaPaintbrush,
+} from "react-icons/fa6";
+
+function SettingLinkButton({ children, to, icon }) {
+  return (
+    <NavLink to={to}>
+      <Button
+        size={"lg"}
+        variant={"ghost"}
+        w={"100%"}
+        justifyContent={"start"}
+        borderRadius={0}
+      >
+        <HStack px={"2"} spacing={"4"} fontSize={"lg"} fontWeight={"medium"}>
+          <Icon fontSize={"2xl"}>{icon && icon}</Icon>
+          <Text>{children}</Text>
+        </HStack>
+      </Button>
+    </NavLink>
+  );
+}
 
 // Page.
 function Settings() {
+  const { isDark } = useThemeInfo();
   return (
-    <Stack>
-      <NavLink to={"/settings/account"}>
-        <Button>Account</Button>
-      </NavLink>
-      <NavLink to={"/settings/profile"}>
-        <Button>Profile</Button>
-      </NavLink>
-      <NavLink to={"/settings/theme"}>
-        <Button>Theme</Button>
-      </NavLink>
-    </Stack>
+    <Box w={"100%"} pt={5} opacity={isDark ? 0.9 : 0.6}>
+      <Stack spacing={0}>
+        <SettingLinkButton icon={<FaUserGear />} to={"/settings/account"}>
+          Account
+        </SettingLinkButton>
+        <SettingLinkButton icon={<FaUserPen />} to={"/settings/profile"}>
+          Profile
+        </SettingLinkButton>
+        <SettingLinkButton icon={<FaPaintbrush />} to={"/settings/theme"}>
+          Theme
+        </SettingLinkButton>
+      </Stack>
+    </Box>
   );
 }
 
