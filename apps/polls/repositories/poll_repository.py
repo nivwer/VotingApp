abc11a19203dea8d106e8694ea358ed59380ff7d
nivwer <nivwerr@gmail.com>
2024-01-02 21:47:21 -0300

    updated poll views.

--- apps/polls/repositories/poll_repository.py
+++ apps/polls/repositories/poll_repository.py
@@ -1,17 +1,34 @@
 from bson import BSON
 from bson.objectid import ObjectId
+
 from rest_framework.exceptions import NotFound
+
+from pymongo import DESCENDING
+
 from utils.mongo_connection import MongoDBSingleton
 
 
 class PollRepository:
+    """
+    A repository class for managing polls in the database.
+
+    This class provides methods to interact with the 'polls_db' MongoDB collection,
+    allowing the application to fetch, create, update, and delete poll data.
+    """
+
     polls_db = MongoDBSingleton().client["polls_db"]
 
     async def create(self, data: dict) -> ObjectId | None:
+        """
+        Creates a new poll.
+        """
         result = await self.polls_db.polls.insert_one(data)
         return result.inserted_id
 
     async def get_by_id(self, id: str, raise_exception: bool = True) -> BSON | None:
+        """
+        Retrieves a poll based on its ID.
+        """
         poll: BSON = await self.polls_db.polls.find_one({"_id": ObjectId(id)})
 
         if not poll:
@@ -22,6 +39,17 @@ class PollRepository:
         return poll
 
     async def update(self, id: str, data: dict, add_options: list, del_options: list):
+        """
+        Updates a poll.
+
+        Args
+            id (str): The ID of the poll to update.
+            data (dict): A dictionary containing the data to update in the poll.
+            add_options (list): A list of options (objects) to add to the poll.
+            del_options (list): A list of option texts to remove from the poll.
+
+        Option object example: { user_id: int, option_text: str, votes: 0 }
+        """
         async with await MongoDBSingleton().client.start_session() as session:
             async with session.start_transaction():
                 # Update poll document in polls collection.
@@ -31,7 +59,7 @@ class PollRepository:
                     session=session,
                 )
 
-                # Add options.
+                # Add options in the poll document.
                 if add_options:
                     await self.polls_db.polls.update_one(
                         {"_id": ObjectId(id)},
@@ -39,7 +67,7 @@ class PollRepository:
                         session=session,
                     )
 
-                # Remove options.
+                # Remove options from the poll document.
                 if del_options:
                     await self.polls_db.polls.update_one(
                         {"_id": ObjectId(id)},
@@ -54,6 +82,9 @@ class PollRepository:
         return ObjectId(id)
 
     async def delete(self, id: str, poll: dict):
+        """
+        Deletes a poll and its associated comments.
+        """
         async with await MongoDBSingleton().client.start_session() as session:
             async with session.start_transaction():
                 # Remove the poll document in the polls collection.
@@ -74,3 +105,32 @@ class PollRepository:
                 await session.end_session()
 
         return ObjectId(id)
+
+    async def add_option(self, id: str, option: dict):
+        # Add the option in the poll document.
+        await self.polls_db.polls.update_one({"_id": ObjectId(id)}, {"$push": {"options": option}})
+
+        return ObjectId(id)
+
+    async def del_option(self, id: str, option: str):
+        # Remove the option from the poll document.
+        await self.polls_db.polls.update_one(
+            {"_id": ObjectId(id)},
+            {"$pull": {"options": {"option_text": option}}},
+        )
+
+        return ObjectId(id)
+
+    async def get_user_poll_list(self, id: int, user_id: int) -> list[BSON]:
+        polls: list = await self.polls_db.polls.find(
+            {
+                "user_id": id,
+                "$or": [
+                    {"privacy": "public"},
+                    {"privacy": "private", "user_id": user_id},
+                ],
+            },
+            sort=[("created_at", DESCENDING)],
+        ).to_list(length=None)
+
+        return polls
--- apps/polls/views/poll_views.py
+++ apps/polls/views/poll_views.py
@@ -1,3 +1,5 @@
+from bson.objectid import ObjectId
+
 from rest_framework import status
 from rest_framework.exceptions import ValidationError, PermissionDenied, NotFound
 from rest_framework.response import Response
@@ -6,28 +8,75 @@ from rest_framework.authentication import SessionAuthentication
 
 from adrf.views import APIView
 
-from apps.polls.services.poll_services import PollService
+from apps.polls.services.poll_service import PollService
 
 
 class PollAPIView(APIView):
+    """
+    Allows authenticated users to create, read, update, and delete polls.
+
+    Requires session-based authentication using SessionAuthentication.
+    Unauthenticated users have read-only permissions.
+    """
+
     authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+
     service = PollService()
 
     async def post(self, request):
+        """
+        Allows authenticated users to create a new poll with specified options.
+
+        Request Data:
+            Expects JSON data with the following structure...
+
+            {
+                "title": "Title",
+                "description": "Description",
+                "privacy": "public/private",
+                "category": "Category",
+                "options": {
+                    "add_options": ["Option 1", "Option 2", ...]
+                }
+            }
+
+        Responses:
+            - 201 Created: Poll created successfully. Includes the ID of the created poll.
+            - 400 Bad Request: Validation errors in the request data.
+        """
+        user_id: int = request.user.id
+        data: dict = request.data
+
         try:
-            id = await self.service.create(user_id=request.user.id, data=request.data)
+            id: ObjectId = await self.service.create(user_id=user_id, data=data)
 
         except ValidationError as error:
             return Response(data=error.detail, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data={"id": str(id)}, status=status.HTTP_201_CREATED)
 
-    async def get(self, request, id):
+    async def get(self, request, id: str):
+        """
+        Fetches and displays details of a poll using the provided poll ID.
+         Returns a JSON response with poll details and user actions.
+
+        Path Parameters:
+            id (required): The ID of the poll.
+
+        Access Control:
+            For private polls, only the owner can access the information.
+
+        Responses:
+            - 200 OK: Poll details and actions of the authenticated user.
+            - 400 Bad Request: Invalid poll ID.
+            - 403 Forbidden: Permission issues (private poll access).
+            - 404 Not Found: Poll not found.
+        """
+        user_id: int = request.user.id
+
         try:
-            poll, user_actions = await self.service.get_by_id(
-                id=id, user_id=request.user.id
-            )
+            poll, user_actions = await self.service.get_by_id(id=id, user_id=user_id)
 
         except ValidationError as error:
             return Response(data=error.detail, status=status.HTTP_400_BAD_REQUEST)
@@ -43,11 +92,42 @@ class PollAPIView(APIView):
             status=status.HTTP_200_OK,
         )
 
-    async def patch(self, request, id):
+    async def patch(self, request, id: str):
+        """
+        Allows authenticated users who own a poll to update its details, including adding or removing options.
+
+        Path Parameters:
+            id (required): The ID of the poll.
+
+        Request Data:
+            Expects JSON data with the following structure...
+
+            {
+                "title": "Updated Poll Title",
+                "description": "Updated Poll Description",
+                "privacy": "public/private",
+                "category": "Updated Poll Category",
+                "options": {
+                    "add_options": ["New Option 1", "New Option 2", ...],
+                    "del_options": ["Option to Remove 1", "Option to Remove 2", ...]
+                }
+            }
+
+        Add and Remove Options:
+            : Adds new options to the poll document.
+            : Removes specified options from the poll document.
+
+        Responses:
+            - 200 OK: Poll updated successfully. Includes the ID of the updated poll.
+            - 400 Bad Request: Validation errors in the request data.
+            - 403 Forbidden: Permission denied if the authenticated user is not the owner.
+            - 404 Not Found: Poll not found.
+        """
+        user_id: int = request.user.id
+        data: dict = request.data
+
         try:
-            id = await self.service.update(
-                id=id, user_id=request.user.id, data=request.data
-            )
+            id: ObjectId = await self.service.update(id=id, user_id=user_id, data=data)
 
         except ValidationError as error:
             return Response(data=error.detail, status=status.HTTP_400_BAD_REQUEST)
@@ -60,9 +140,26 @@ class PollAPIView(APIView):
 
         return Response(data={"id": str(id)}, status=status.HTTP_200_OK)
 
-    async def delete(self, request, id):
+    async def delete(self, request, id: str):
+        """
+        Allows authenticated users who own a poll to delete it, along with its associated comments.
+
+        Path Parameters:
+            id (required): The ID of the poll.
+
+        Remove Comment Documents:
+            Deletes comment documents associated with the poll from the comments collection.
+
+        Responses:
+            - 200 OK: Poll and associated comments removed successfully. Includes the ID of the deleted poll.
+            - 400 Bad Request: Validation errors in the request data.
+            - 403 Forbidden: Permission denied if the authenticated user is not the owner.
+            - 404 Not Found: Poll not found.
+        """
+        user_id: int = request.user.id
+
         try:
-            id = await self.service.delete(id=id, user_id=request.user.id)
+            id: ObjectId = await self.service.delete(id=id, user_id=user_id)
 
         except ValidationError as error:
             return Response(data=error.detail, status=status.HTTP_400_BAD_REQUEST)
