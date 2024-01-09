0612f70b2676d3d9eaaa66888ef223764220c67b
nivwer <nivwerr@gmail.com>
2024-01-09 12:54:16 -0300

    modified MongoDBSingleton.

--- config/middleware/mongo_middleware.py
+++ config/middleware/mongo_middleware.py
@@ -1,7 +1,6 @@
-# MongoDB connection using Singleton.
 from utils.mongo_connection import MongoDBSingleton
 
-# MongoDB Middleware.
+
 class MongoDBMiddleware:
     def __init__(self, get_res):
         self.get_res = get_res
--- utils/mongo_connection.py
+++ utils/mongo_connection.py
@@ -3,10 +3,26 @@ from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import errors
 
+# environment
 load_dotenv()
 
 
 class MongoDBSingleton:
+    """
+    Singleton class for managing a single MongoDB connection instance.
+
+    This class ensures that only one instance of the MongoDB connection is created and
+    provides a centralized point for accessing the connection throughout the application.
+
+    Usage:
+    ```
+    mongo_singleton = MongoDBSingleton()
+    mongo_client = mongo_singleton.client
+    ```
+
+    Note: The MongoDB connection URI is expected to be provided as an environment variable 'MONGO_URI'.
+    """
+
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -16,23 +32,25 @@ class MongoDBSingleton:
         return cls._instance
 
     def _initialize_connection(self):
-        MONGO_URI = os.getenv("MONGO_URI")
+        MONGO_URI: str = os.getenv("MONGO_URI")
 
         try:
             self.client = AsyncIOMotorClient(MONGO_URI)
-
-            print('"MongoDB connection successful..."')
+            message: str = "MongoDB connection successful..."
+            print(f"{message}")
 
         except errors.ServerSelectionTimeoutError as e:
-            print(
-                f"{self._get_current_timestamp()} MongoDB server selection timeout error: {e}"
-            )
+            message: str = "MongoDB server selection timeout error: "
+            print(f"{message}{e}")
 
         except errors.ConnectionFailure as e:
-            print(f"{self._get_current_timestamp()} MongoDB connection error: {e}")
+            message: str = "MongoDB connection error: "
+            print(f"{message}{e}")
 
         except errors.InvalidURI as e:
-            print(f"{self._get_current_timestamp()} MongoDB Invalid URI error: {e}")
+            message: str = "MongoDB Invalid URI error: "
+            print(f"{message}{e}")
 
         except errors.ConfigurationError as e:
-            print(f"{self._get_current_timestamp()} MongoDB configuration error: {e}")
+            message: str = "MongoDB configuration error: "
+            print(f"{message}{e}")
