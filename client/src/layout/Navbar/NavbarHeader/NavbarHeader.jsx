cf4b92d4a383bd7f60e2e162565207bee0d7cb08
nivwer <nivwerr@gmail.com>
2024-01-18 15:06:07 -0300

    removed NavLeftDrawer component.

--- client/src/layout/Navbar/NavbarHeader/NavbarHeader.jsx
+++ client/src/layout/Navbar/NavbarHeader/NavbarHeader.jsx
@@ -1,32 +1,29 @@
 import { useThemeInfo } from "../../../hooks/Theme";
-import { Box, HStack, Heading, IconButton, Show, Text } from "@chakra-ui/react";
+import { Box, HStack, Show, Text } from "@chakra-ui/react";
 import { NavLink, useLocation } from "react-router-dom";
 import NavbarBreadcrumb from "./NavbarBreadcrumb/NavbarBreadcrumb";
-import { FaBars } from "react-icons/fa6";
 
-function NavbarHeader({ disclosure }) {
+function NavbarHeader() {
   const { isDark } = useThemeInfo();
-  const { onOpen } = disclosure;
   const location = useLocation();
   return (
-      <HStack opacity={1} spacing={5}>
-        <NavLink to={"/home"}>
-          <Text children={"VotingApp"} fontSize="xl" fontWeight={"bold"} />
-        </NavLink>
-        {location.pathname !== "/signup" && location.pathname !== "/signin" && (
-          <Show above="md">
-            <Box
-              bg={isDark ? "gothicPurpleAlpha.100" : "gothicPurpleAlpha.200"}
-              p={"5px"}
-              px={"25px"}
-              borderRadius={"3xl"}
-              boxShadow={"base"}
-            >
-              <NavbarBreadcrumb />
-            </Box>
-          </Show>
-        )}
-      </HStack>
+    <HStack opacity={1} spacing={5}>
+      <NavLink to={"/home"}>
+        <Text children={"VotingApp"} fontSize="xl" fontWeight={"bold"} />
+      </NavLink>
+      {location.pathname !== "/signup" && location.pathname !== "/signin" && (
+        <Show above="md">
+          <Box
+            bg={isDark ? "gothicPurpleAlpha.100" : "gothicPurpleAlpha.200"}
+            p={"5px"}
+            px={"25px"}
+            borderRadius={"3xl"}
+            boxShadow={"base"}
+            children={<NavbarBreadcrumb />}
+          />
+        </Show>
+      )}
+    </HStack>
   );
 }
 
