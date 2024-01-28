5063c4c7609f620f3250131a6c7c31e908ebfc65
nivwer <nivwerr@gmail.com>
2024-01-28 07:44:02 -0300

    fixed: PollComments component.

--- client/src/pages/Poll/PollComments/PollComments.jsx
+++ client/src/pages/Poll/PollComments/PollComments.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
 import { useAddCommentMutation, useReadCommentsQuery } from "../../../api/pollsAPISlice";
 import { useSelector } from "react-redux";
-import Cookies from "js-cookie";
 import CommentCard from "../../../components/Cards/CommentCard/CommentCard";
 import Pagination from "../../../components/Pagination/Pagination";
 import CommentInput from "../../../components/Form/CommentInput/CommentInput";
 
 function PollComments({ id }) {
-  const csrftoken = Cookies.get("csrftoken");
-  const { isAuthenticated } = useSelector((state) => state.session);
+  const { isAuthenticated, csrftoken } = useSelector((state) => state.session);
   const [dataQuery, setDataQuery] = useState(false);
   const [resetValues, setResetValues] = useState(false);
 
